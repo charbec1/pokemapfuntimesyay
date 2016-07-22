60c9b92b071327efadca443af53ea713ddee1184
charbec1 <cole.harbeck@gmail.com>
2016-07-22 12:33:03 -0400

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,7 +5,7 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'locales',
     'ROOT_PATH': None,
-    'ORIGINAL_LATITUDE': None,
-    'ORIGINAL_LONGITUDE': None,
+    'ORIGINAL_LATITUDE': 40.912356,
+    'ORIGINAL_LONGITUDE': -73.957198,
     'GMAPS_KEY': None
 }
